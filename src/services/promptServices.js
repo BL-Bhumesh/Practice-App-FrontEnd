9a6b81769307bc9f71c840d4d9898e225d1f0887
Bhumesh Ranjane <bhumeshranjane@Bhumeshs-MacBook-Pro.local>
2025-10-15 11:35:58 +0530

    service file changes

--- src/services/promptServices.js
+++ src/services/promptServices.js
@@ -1,6 +1,6 @@
 import axiosInstance from './axiosServices';
 
-  export const getPrompt = (answer_type) => {
-  return axiosInstance.get(`/prompts/default-prompt?answer_type=${answer_type}`);
-};
 
+
+export const getPrompt = (answer_type) =>
+  axiosInstance.get(`/prompts/default-prompt?answer_type=${answer_type}&_t=${Date.now()}`);
--- src/services/questionService.js
+++ src/services/questionService.js
@@ -1,8 +1,11 @@
 import axiosInstance from './axiosServices';
 
-export const getQuestions = (answer_type, skip = 0, limit = 1) =>
-  axiosInstance.get(
-    `/questions/get_questions?answer_type=${answer_type}&skip=${skip}&limit=${limit}`
-  );
+// export const getQuestions = (answer_type, skip = 0, limit = 1) =>
+//   axiosInstance.get(
+//     `/questions/get_questions?answer_type=${answer_type}&skip=${skip}&limit=${limit}&_t=${Date.now()`
+//   );
 
-  
+  export const getQuestions = (answer_type, skip = 0, limit = 1) =>
+  axiosInstance.get(
+    `/questions/get_questions?answer_type=${answer_type}&skip=${skip}&limit=${limit}&_t=${Date.now()}`
+  );
